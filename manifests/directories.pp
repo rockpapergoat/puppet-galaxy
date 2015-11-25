666205e2748008241e36700e636e61d5ee75c615
nate <n8@adullmoment.com>
2015-11-24 22:33:47 -0500

    use the dirtree module to create the directories

--- manifests/directories.pp
+++ manifests/directories.pp
@@ -1,24 +1,28 @@
 # Class: galaxy::directories
 # inherits galaxy::params, where we set a number of paths needed here.
+# uses the dirtree module here: https://github.com/AlexCline/dirtree
 #
 class galaxy::directories (
   $app_dir             = $galaxy::params::app_dir,
-  $cluster_files       = "${app_dir}/database/pbs",
-  $compiled_templates  = "${app_dir}/database/compiled_templates",
-  $file_path           = "${app_dir}/database/files",
-  $ftp                 = "${app_dir}/database/ftp/",
+  $db_path             = "${app_dir}/database",
+  $cluster_files       = "${db_path}/pbs",
+  $compiled_templates  = "${db_path}/compiled_templates",
+  $file_path           = "${db_path}/files",
+  $ftp                 = "${db_path}/ftp/",
   $genetrack_plots     = "${app_dir}/static/genetrack/plots",
-  $job_working         = "${app_dir}/database/job_working_directory",
-  $object_store_cache  = "${app_dir}/database/object_store_cache",
-  $object_store_object = "${app_dir}/database/object_store_object",
-  $tmp_file            = "${app_dir}/database/tmp",
+  $job_working         = "${db_path}/job_working_directory",
+  $object_store_cache  = "${db_path}/object_store_cache",
+  $object_store_object = "${db_path}/object_store_object",
+  $tmp_file            = "${db_path}/tmp",
   $tool_data_jars      = "${app_dir}/tool-data/shared/jars",
   $tool_dependency     = "${app_dir}/tool_dependencies",
   $whoosh_index        = "${app_dir}/database/whoosh_indexes",
   ) inherits galaxy::params {
   $paths = [
+    $app_dir,
     $cluster_files,
     $compiled_templates,
+    $db_path,
     $file_path,
     $ftp,
     $genetrack_plots,
@@ -30,7 +34,7 @@ class galaxy::directories (
     $tool_dependency,
     $whoosh_index,
   ]
- file  { $paths:
+ file  { dirtree($paths):
     before => Class['galaxy::universe'],
     ensure => directory,
     owner  => $galaxy_user,
