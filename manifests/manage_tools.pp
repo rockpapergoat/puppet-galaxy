1c8907e4b6d11cb56ac774ad58b5638bd7b7a0c4
nate <n8@adullmoment.com>
2015-11-25 11:21:34 -0500

    use the user/group params here, too

--- manifests/manage_tools.pp
+++ manifests/manage_tools.pp
@@ -4,15 +4,17 @@
 #
 #
 class galaxy::manage_tools (
-  $directory = $galaxy::params::app_directory,
-  $cmd       = "python $directory/scripts/manage_tools.py",
-  ){
+  $directory    = $galaxy::params::app_directory,
+  $cmd          = "python $directory/scripts/manage_tools.py",
+  $galaxy_user  = $galaxy::params::galaxy_user,
+  $galaxy_group = $galaxy::params::galaxy_group,
+  ) inherits galaxy::params {
   exec { 'Manage Tools Upgrade':
     path     => '/usr/bin:/usr/sbin:/bin:/sbin',
     cwd      => $directory,
     command  => "$cmd upgrade",
-    user     => 'galaxy',
-    group    => 'galaxy',
+    user     => $galaxy_user,
+    group    => $galaxy_group,
     unless   => "test \"\$($cmd db_version)\" -eq \"\$($cmd version)\"",
     provider => shell,
     require  => Class['galaxy::create_db'],
